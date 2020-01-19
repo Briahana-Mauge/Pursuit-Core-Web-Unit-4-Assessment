e379bca3b469edd71f3e96cc6895ba2d2d607093
Bpmauge <briahanamauge@pursuit.org>
2020-01-19 14:31:24 -0500

    function to grab video based on search

--- youtube/src/Components/Home.jsx
+++ youtube/src/Components/Home.jsx
@@ -1,15 +1,69 @@
-import React from 'react'
-const Home = () => {
-    return (
-        <>
-        <form>
-            <input type = 'text' placeholder = 'Search for a video'></input>
-            <input type = 'submit' value = 'Search'></input>
-        </form>
+import React from 'react';
+import axios from 'axios';
+import Result from './Result';
 
-        <p>No Search Results. Search for videos above! </p>
-        </>
-    )
+class Home extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            search: '',
+            loading: true,
+            results: [],
+            ids: []
+        }
+    }
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        const { search, ids } = this.state;
+        // console.log(search)
+        let videoIDs = [];
+       
+        let searchAPI = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&order=relevance&key=AIzaSyAOnB1HKhP0_bepRLasgXrP7y7o21EXFSg&q=${search}&type=video`
+        let res = await axios.get(searchAPI)
+        videoIDs = res.data.items
+        console.log(videoIDs)
+        for (let i = 0; i < videoIDs.length; i++) {
+            ids.push({
+                id: `${videoIDs[i].id.videoId}`,
+                title: `${videoIDs[i].snippet.title}`,
+                thumbnail: `${videoIDs[i].snippet.thumbnails.medium.url}`,
+                width: `${videoIDs[i].snippet.thumbnails.medium.width}`,
+                height: `${videoIDs[i].snippet.thumbnails.medium.height}`
+            })
+            console.log(ids)
+        }
+        this.setState({
+            ids: ids,
+            loading: false
+        })
+    }
+    handleSearchInput = (event) => {
+        const { ids } = this.state
+        this.setState({
+            search: event.target.value,
+            ids: [],
+            loading: true
+        })
+
+    }
+    render = () => {
+        const { ids, loading } = this.state
+        return (
+            <>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' placeholder='Search for a video' onChange={this.handleSearchInput}></input>
+                    <input type='submit' value='Search'></input>
+                </form>
+                {loading ?
+                    <p>No Search Results. Search for videos above! </p> :
+                    <Result ids={ids} />}
+
+
+
+
+            </>
+        )
+    }
 }
 
 export default Home;
