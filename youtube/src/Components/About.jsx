68dc96d5998f909e2751b6d7d515661cea3c8a79
Bpmauge <briahanamauge@pursuit.org>
2020-01-19 17:26:37 -0500

    added an about message

--- youtube/src/Components/About.jsx
+++ youtube/src/Components/About.jsx
@@ -3,7 +3,10 @@ const About = () => {
     return (
         <>
         <h1>About</h1>
-        <p>Learning software engineering</p>
+        <p>This youtube clone was poorly built by Briahana as part of her unit 4 assessment.</p>
+         <p>Technologies used for this project include Youtube's API, React, CSS and several npm packages, like 'react-youtube'. </p>
+        <p> On the home page, you can search for video and select your pick from the first 8 choices from your search. </p>
+        <p>Clicking on the video takes you to the video's page where you can watch and hopefully one day can leave comments. </p>
         </>
     )
 }
