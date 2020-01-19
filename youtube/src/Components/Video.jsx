0fe622de3015b7cbbe903cbb9f5980bd0048260a
Bpmauge <briahanamauge@pursuit.org>
2020-01-19 15:19:27 -0500

    added made a class component for video

--- youtube/src/Components/Video.jsx
+++ youtube/src/Components/Video.jsx
@@ -1,11 +1,53 @@
 import React from 'react'
-const Video = () => {
-    return (
-        <>
-        <h1>Video</h1>
-        <p>Welcome to Briahana's personal website</p>
-        </>
-    )
+import YouTube from 'react-youtube';
+class Video extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            id: props.location.pathname.replace(`/video/`, ``)
+        }
+    }
+    ready = (event) => {
+        event.target.pauseVideo()
+    }
+    render() {
+        const opts = {
+            height: `390`,
+            width: `640`,
+        }
+        const { id } = this.state
+        return (
+            <>
+                <YouTube videoId={id}
+                    opts={opts}
+                    onReady={this.ready} />
+                <form>
+                    <input type='text' placeholder='name' />
+                    <input type='text' placeholder='comment' />
+                    <input type='submit' value='Submit' />
+                </form>
+            </>
+        )
+    }
 }
+
+// const Video = (props) => {
+//     console.log(props)
+//     let videoId = props.location.pathname.replace(`/video/`, ``)
+//     console.log(props, videoId)
+//     return (
+//         <>
+//             {/* <Youtube/> */}
+//             <h1>Video</h1>
+//             <p>Welcome to Briahana's personal website</p>
+
+//             <form>
+//                 <input type='text' placeholder='name' />
+//                 <input type='text' placeholder='comment' />
+//                 <input type='submit' value='Submit' />
+//             </form>
+//         </>
+//     )
+// }
 
 export default Video;
